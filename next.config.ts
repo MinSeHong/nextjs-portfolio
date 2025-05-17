6056ac5ecc627fb3223c59b596270bde7778d86f
MinSeHong <rebellia@naver.com>
2025-05-17 09:46:23 +0900

    hompage와 경로 레파지토리로 설정

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from "next";
+
 const nextConfig: NextConfig = {
     typescript: {
     // 빌드 중에 타입스크립트 오류를 무시하도록 설정
@@ -7,13 +8,11 @@ const nextConfig: NextConfig = {
     eslint: {
       ignoreDuringBuilds: true, // 빌드 중 ESLint 오류를 무시
     },
-    output:'export',
-    
     assetPrefix:
       process.env.NODE_ENV === "production"
-        ? "https://github.com/MinSeHong/MinSeHong.github.io/"
-        : ""
-    
+        ? "https://minsehong.github.io/MinSeHong.github.io"
+        : "",
+      output:'export'
 };
 
 export default nextConfig;

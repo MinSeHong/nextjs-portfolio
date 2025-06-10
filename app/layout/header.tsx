af40d0c32c1ef35ebd3b277544902e6a77f111c8
MinSeHong <rebellia@naver.com>
2025-06-11 04:33:15 +0900

    전체 디자인 수정

--- app/layout/header.tsx
+++ app/layout/header.tsx
@@ -2,5 +2,28 @@
 import styles from './header.module.scss';
 
 export default function Header() {
-  return <header className={styles.header}>Header</header>;
+  return (
+    <header className={styles.header}>
+      <nav>Intro</nav>
+      <nav>Introduce</nav>
+      <nav>Project</nav>
+      <nav>Personality</nav>
+
+      <div className={styles.iconBox}>
+        <div>
+          <a
+            href="https://www.notion.so/minsehong/Story-of-Minsehong-2044339956d080119278d19d365a900e?source=copy_link"
+            target="_blank"
+          >
+            <img src="./icons/notion-icon.png" />
+          </a>
+        </div>
+        <div>
+          <a href="https://github.com/MinSeHong" target="_blank">
+            <img src="./icons/github-icon.svg" />
+          </a>
+        </div>
+      </div>
+    </header>
+  );
 }
